c030899007881f48b86f47e814eacd54ccd2161d
Musrea <musreaabdo@gmail.com>
2023-01-14 21:26:23 +0000

    Test modifed notebook

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
     # region register processor yapf: disable
     processors = dict()
     processors['recognition'] = import_class('processor.recognition.REC_Processor')
-    processors['demo_old'] = import_class('processor.demo_old.Demo')
-    processors['demo'] = import_class('processor.demo_realtime.DemoRealtime')
-    processors['demo_offline'] = import_class('processor.demo_offline.DemoOffline')
+    #processors['demo_old'] = import_class('processor.demo_old.Demo')
+    #processors['demo'] = import_class('processor.demo_realtime.DemoRealtime')
+    #processors['demo_offline'] = import_class('processor.demo_offline.DemoOffline')
     #endregion yapf: enable
 
     # add sub-parser
